079c0c976f68cf037d19f9cc66086fc89eeee0cf
siflu <5452820+siflu@users.noreply.github.com>
2022-09-30 13:15:21 +0200

    added in our addToCart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -16,7 +16,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
         </div>
     )
 }
